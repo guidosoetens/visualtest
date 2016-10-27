fef65d89d440dbc6237605ee1fea7b4ea6d28b0d
Guido Soetens <guido@activecues.com>
2016-10-27 17:43:52 +0200

    fixed derivatives

--- src/BGCellGenerator.cpp
+++ src/BGCellGenerator.cpp
@@ -1,6 +1,7 @@
 #include "BGCellGenerator.h"
 
 #define TEX_WIDTH 256
+#define MAX_SAMPLE_HEIGHT 0.5
 #define TEX_BUFFER_SIZE (TEX_WIDTH * TEX_WIDTH)
 
 BGCellGenerator::BGCellGenerator() {
@@ -19,19 +20,19 @@ BGCellGenerator::BGCellGenerator() {
 
             float x = 2.0 * scale * (j / (float)(TEX_WIDTH - 1) - .5);
 
-            float deltaSample = .01;
+            float deltaSample = .001;
 
             float h = sampleHeight(x, y);
             float dhx = sampleHeight(x + deltaSample, y) - sampleHeight(x - deltaSample, y);
             float dhy = sampleHeight(x, y + deltaSample) - sampleHeight(x, y - deltaSample);
 
             ofVec3f u(2 * deltaSample, 0, dhx);
-            ofVec3f v(0, 2 * deltaSample, dhx);
+            ofVec3f v(0, 2 * deltaSample, dhy);
             ofVec3f normal = u.cross(v);
             normal.normalize();
 
             int idx = 3 * (i * TEX_WIDTH + j);
-            float rgb[3] = { .5 + .5 * normal.x, 0, 0 };
+            float rgb[3] = { .5 + .5 * normal.x, .5 + .5 * normal.y, normal.z };
             for(int c=0; c<3; ++c)
                 pixels[idx + c] = rgb[c] * 255;
         }
@@ -55,7 +56,7 @@ BGCellGenerator::sampleHeight(float x, float y) {
     float len = to.length();
     float t = fmaxf(0.0, fminf(maxDist, len)) / maxDist;
 
-    return (.5 + .5 * cosf(t * M_PI)) * .5 * TEX_WIDTH;
+    return (.5 + .5 * cosf(t * M_PI)) * MAX_SAMPLE_HEIGHT;
 }
 
 void BGCellGenerator::draw() {
